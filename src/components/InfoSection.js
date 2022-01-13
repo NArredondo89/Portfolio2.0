c1c391fd7b54fe4e8860f31ec1716071b0796792
Nick <nickarredondo@LAPTOP-2V3732UN.localdomain>
2022-01-12 22:30:07 -0800

    Used Map to add span tags to string to add hover effect on all words

--- src/components/InfoSection.js
+++ src/components/InfoSection.js
@@ -9,50 +9,43 @@ function InfoSection() {
     Aos.init({});
   }, []);
 
+  const string = `I am a Full-Stack Engineer who is eager to prove value within an established company. Motivated to build efficient and scalable products, focusing on enhancing user experiences. I enjoy working with cross functional teams adapting my skill set to provide support the team needs in order to succeed. My background in kinesiology has provided me with a strong foundation in problem solving skills with the ability to adapt solutions to fit clients needs.`;
+
+  const string2 = `When I am not coding or learning, I'm usually traveling playing
+  new disc golf courses throughout California. I also like to
+  skydive, hike and try out new resturants!`;
+
+  const stringParts = string.split(" ");
+  const stringPartstwo = string2.split(" ");
+
+  const wrappedLetters = stringParts.map((stringletters, index) => {
+    return <span>{stringletters}</span>;
+  });
+  const wrappedLetterstwo = stringPartstwo.map((stringletterstwo, index) => {
+    return <span>{stringletterstwo}</span>;
+  });
+
   return (
     <>
       <div className="info__container">
         <div className="info__word__container">
-          <div data-aos="zoom-in-down" className="info__text">
-            <span>I</span>
-            <span>am</span>
-            <span>a</span>
-            <span>Full</span>
-            <span>Stack</span>
-            <span>Software</span>
-            <span>Engineer</span>
-            <span>who</span>
-            <span>loves</span>
-            <span>to</span>
-            <span>make</span>
-            <span>beautiful</span>
-            <span>and</span>
-            <span>unique</span>
-            <span>websites</span>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span> I am interested in leveraging technology to improve
-            the quality of life in areas such as education, environment, health
-            and wellness. My background in kinesiology has provided me with a
-            strong foundation in problem solving skills with the ability to
-            adapt solutions to fit client needs.
-            <div className="info__text__two">
-              {" "}
-              When I am not coding or learning, I'm usually traveling playing
-              new disc golf courses throughout California. I also like to
-              skydive, hike and try out new resturants!
-            </div>
+          <div
+            data-aos="zoom-in-down"
+            data-aos-duration="3000"
+            className="info__text"
+          >
+            {wrappedLetters}
+          </div>
+          <div
+            data-aos="zoom-in-down"
+            data-aos-duration="3000"
+            className="info__text__two"
+          >
+            {wrappedLetterstwo}
           </div>
         </div>
       </div>
+      <div className="iconContainer"></div>
     </>
   );
 }
